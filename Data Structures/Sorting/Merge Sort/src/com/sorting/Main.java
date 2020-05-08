64a6c8acc11cf8dc20037b9101fb2764cdfbe936
AnastasiosMargaritis <58393884+AnastasiosMargaritis@users.noreply.github.com>
2020-05-08 16:01:54 +0300

    Implementing Merge Sort

--- Data Structures/Sorting/Merge Sort/src/com/sorting/Main.java
+++ Data Structures/Sorting/Merge Sort/src/com/sorting/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	    int[] array = {-15, 11, 7, 1 ,16, 4, 35, 666, 42, -97, -12, 9, 0, 31};
 
+        mergeSort(array, 0, array.length);
 
 	    for(int i = 0; i < array.length; i++){
             System.out.println(array[i]);
@@ -21,6 +22,30 @@ public class Main {
 
         int mid = (start + end) / 2;
 
-        
+        mergeSort(input, start, mid);
+        mergeSort(input, mid, end);
+        merge(input, start, mid, end);
+
+    }
+
+    public static void merge(int[] input, int start, int mid, int end){
+
+        if(input[mid - 1] <= input[mid]){
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end-start];
+
+
+        while(i < mid && j < end){
+            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+        }
+
+        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
     }
 }
