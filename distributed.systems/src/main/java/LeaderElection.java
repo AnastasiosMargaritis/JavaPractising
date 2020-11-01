2e817a75f088bedb9028ce2fbaacb44cd137c5d7
AnastasiosMargaritis <anastasismargaritis@gmail.com>
2020-11-01 14:02:24 +0200

    Adding Fault Tolerance, Leader Reelection logic.

--- distributed.systems/src/main/java/LeaderElection.java
+++ distributed.systems/src/main/java/LeaderElection.java
@@ -1,22 +1,64 @@
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
+import java.lang.annotation.Target;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class LeaderElection implements Watcher {
 
     private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
     private ZooKeeper zooKeeper;
     private static final int SESSION_TIMEOUT = 3000;
+    private static final String ELECTION_NAMESPACE = "/election";
+    private static final String TARGET_ZNODE = "/target_znode";
+    private String currentZnodeName;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
         LeaderElection leaderElection = new LeaderElection();
 
         leaderElection.connectToZookeeper();
+        leaderElection.volunteerForLeadership();
+        leaderElection.reelectLeader();
         leaderElection.run();
         leaderElection.close();
         System.out.println("Disconnected from Zookeeper, exiting application.");
+    }
+
+    public void volunteerForLeadership() throws KeeperException, InterruptedException {
+        String znodePrefix = ELECTION_NAMESPACE + "/c_";
+        String znodeFullPath = zooKeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+
+        System.out.println("znode name " + znodeFullPath);
+        this.currentZnodeName = znodeFullPath.replace(ELECTION_NAMESPACE + "/", "");
+    }
+
+    public void reelectLeader() throws KeeperException, InterruptedException {
+        Stat predeccessorStat = null;
+        String predecessorZnodeName = "";
+
+        while(predeccessorStat == null){
+            List<String> children = zooKeeper.getChildren(ELECTION_NAMESPACE, false);
+
+            Collections.sort(children);
+
+            String smallestChild = children.get(0);
+
+            if(smallestChild.equals(currentZnodeName)){
+                System.out.println("I am the leader, ");
+                return;
+            }else{
+                System.out.println("I am not the leader");
+                int predecessorIndex = Collections.binarySearch(children, currentZnodeName) - 1;
+                predecessorZnodeName = children.get(predecessorIndex);
+                predeccessorStat = zooKeeper.exists(ELECTION_NAMESPACE + "/" + predecessorZnodeName, this);
+            }
+        }
+
+        System.out.println("watching znode " + predecessorZnodeName);
+        System.out.println();
     }
 
     public void connectToZookeeper() throws IOException {
@@ -33,6 +75,19 @@ public class LeaderElection implements Watcher {
         zooKeeper.close();
     }
 
+    public void watchTargetZnode() throws KeeperException, InterruptedException {
+        Stat stat = zooKeeper.exists(TARGET_ZNODE, this);
+
+        if(stat == null){
+            return;
+        }
+
+        byte[] data = zooKeeper.getData(TARGET_ZNODE, this, stat);
+        List<String> children = zooKeeper.getChildren(TARGET_ZNODE, this);
+
+        System.out.println("Data: " + new String(data) + " children: " + children);
+    }
+
     @Override
     public void process(WatchedEvent event){
         switch (event.getType()){
@@ -44,6 +99,15 @@ public class LeaderElection implements Watcher {
                         System.out.println("Disconnected event triggered.");
                         zooKeeper.notifyAll();
                     }
+                }
+                break;
+            case NodeDeleted:
+                try{
+                    reelectLeader();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (KeeperException e) {
+                    e.printStackTrace();
                 }
         }
     }
