23013c1426cc7f8c69b843a8337910535303cf1f
AnastasiosMargaritis <anastasismargaritis@gmail.com>
2020-12-03 17:43:48 +0200

    Enabling JWT with OAuth 2.0 with Microservices.

--- Security/Application/src/main/java/com/app/Application.java
+++ Security/Application/src/main/java/com/app/Application.java
@@ -1,12 +1,40 @@
 package com.app;
 
+import com.app.config.ServiceConfig;
+import com.app.utils.UserContextInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootApplication
 @EnableResourceServer
 public class Application {
+
+	@Autowired
+	private ServiceConfig serviceConfig;
+
+	@Primary
+	@Bean
+	public RestTemplate getCustomRestTemplate(){
+		RestTemplate restTemplate = new RestTemplate();
+		List interceptors = restTemplate.getInterceptors();
+
+		if(interceptors == null){
+			restTemplate.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+		}else{
+			interceptors.add(new UserContextInterceptor());
+			restTemplate.setInterceptors(interceptors);
+		}
+
+		return restTemplate;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
---
+++ Security/Application/src/main/java/com/app/config/ServiceConfig.java
@@ -0,0 +1,16 @@
+package com.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration
+public class ServiceConfig {
+
+    private String jwtSigningKey="345345fsdfsf5345";
+
+
+    public String getJwtSigningKey() {
+        return jwtSigningKey;
+    }
+}
